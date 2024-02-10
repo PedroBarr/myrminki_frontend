fae85dbda200bc78fa65c441e9db52636e4bd9c3
PedroBarr <pebarreraa@udistrital.edu.co>
2024-02-09 23:35:06 -0500

    Agregar comportamiento de carrusel (acerca de)

--- src/app/core/pages/page-about/page-about.component.ts
+++ src/app/core/pages/page-about/page-about.component.ts
@@ -1,11 +1,70 @@
 import {
-    Component
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
 } from '@angular/core';
+
+import {
+  register as swiperRegister,
+} from 'swiper/element/bundle';
+
+
+
+swiperRegister();
+
+
 
 @Component({
   selector: 'myrmex-page-about',
   templateUrl: './page-about.component.html',
   styleUrls: ['./page-about.component.scss'],
 })
+export class PageAboutComponent implements AfterViewChecked, OnInit {
 
-export class PageAboutComponent { }
+  total_slides: number = 1;
+  current_slide: number = 1;
+  slides: any[] = [];
+
+  @ViewChild('swiper') swiperRef: ElementRef | undefined;
+
+  constructor( ) { }
+
+  ngOnInit() {
+    this.loadSlides();
+    this.current_slide = 0;
+  }
+
+  ngAfterViewChecked(): void {
+    this.swiperRef?.nativeElement.swiper.update();
+  }
+
+  swiperSlideChanged ( ) {
+    this.current_slide = this.swiperRef?.nativeElement.swiper.activeIndex;
+  }
+
+  setSlide(index: number) {
+    this.current_slide = index + 1;
+  }
+
+  /**
+  * Go to next slide
+  */
+  navNext() {
+    this.swiperRef?.nativeElement.swiper.update();
+    if (this.current_slide == this.total_slides - 1) {
+        this.current_slide = 0;
+    } else {
+        this.current_slide += 1;
+    }
+  }
+
+  /**
+  * Load slides from API
+  */
+  async loadSlides() {
+    this.total_slides = 1;
+  }
+
+}
--- src/app/core/pages/pages.module.ts
+++ src/app/core/pages/pages.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
+import {
+  NgModule,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
 
 import { PagesRoutingModule } from './pages-routing.module';
 
-import { PageAboutComponent } from './page-about/page-about.component';
-import { PageMainComponent } from './page-main/page-main.component';
-import { PageMainSetUpComponent } from './page-main-set-up/page-main-set-up.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {
+  PageAboutComponent
+} from './page-about/page-about.component';
+
+import {
+  PageMainComponent
+} from './page-main/page-main.component';
+
+import {
+  PageMainSetUpComponent
+} from './page-main-set-up/page-main-set-up.component';
+
+import {
+  PageNotFoundComponent
+} from './page-not-found/page-not-found.component';
+
+
 
 @NgModule({
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+  ],
   declarations: [
     PageAboutComponent,
     PageMainComponent,
