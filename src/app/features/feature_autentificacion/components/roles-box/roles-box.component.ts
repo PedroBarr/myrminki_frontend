6330eb0fdffcb82b3da08e9a5554013478c7b3c9
PedroBarr <pebarreraa@udistrital.edu.co>
2024-09-05 00:01:41 -0500

    Corregir detalles caja de roles

--- src/app/features/feature_autentificacion/components/roles-box/roles-box.component.ts
+++ src/app/features/feature_autentificacion/components/roles-box/roles-box.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import {
+  PermisoTipado,
+} from '../../models/permisos.model';
 
 @Component({
-  selector: 'app-roles-box',
+  selector: 'myrmex-roles-box',
   templateUrl: './roles-box.component.html',
   styleUrls: ['./roles-box.component.scss']
 })
 export class RolesBoxComponent implements OnInit {
+
+  @Input() roles: PermisoTipado[] = [];
 
   constructor() { }
 
