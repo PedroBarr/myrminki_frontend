c04045792a52bf34cc3f6a552170bba2468df59e
PedroBarr <pebarreraa@udistrital.edu.co>
2024-09-04 23:42:56 -0500

    Agregar funcionalidad traer permisos en pagina de perfil

--- src/app/features/feature_autentificacion/pages/page-profile/page-profile.component.ts
+++ src/app/features/feature_autentificacion/pages/page-profile/page-profile.component.ts
@@ -16,6 +16,11 @@ import {
   UsuarioPerfilVistaHabilitada,
 } from '../../models/usuario.model';
 
+import {
+  PermisoTipado,
+  FactoriaPermisosTipados,
+} from '../../models/permisos.model';
+
 import { environment } from 'src/environments/environment';
 
 
@@ -31,6 +36,8 @@ export class PageProfileComponent implements OnInit {
   public tipo_vista: UsuarioPerfilVistaHabilitada =
     UsuarioPerfilVistaHabilitada.none;
 
+  public permisos: PermisoTipado[] = [];
+
   constructor (
     private authIntercepService: AutentificacionInterceptorService,
     private route: ActivatedRoute,
@@ -39,22 +46,26 @@ export class PageProfileComponent implements OnInit {
   async ngOnInit ( ) {
     if (this.route.snapshot.paramMap.get('id') != null) {
       const id = this.route.snapshot.paramMap.get('id');
-      if (id) await this.loadUserProfile(id);
+      if (id) {
+        await this.loadUserProfile(id);
+        await this.loadUserProfilePermissions(id);
+      }
     } else {
       await this.loadProfile();
+      await this.loadProfilePermissions();
     }
   }
 
   /**
   * Load profile from API
   */
-  async loadProfile ( ) {
+  private async loadProfile ( ) {
     const axiosInstance = axios.create();
 
     const intercep_auth_id = this.authIntercepService.addAuthInterceptor(axiosInstance);
     const intercep_error_id = this.authIntercepService.addAuthErrorInterceptor(axiosInstance);
 
-    axiosInstance.get(
+    await axiosInstance.get(
       environment.MYRMEX_API + '/perfil',
     )
       .then(response => {
@@ -86,13 +97,13 @@ export class PageProfileComponent implements OnInit {
   /**
    * Load user profile from API
    */
-  async loadUserProfile (id: string) {
+  private async loadUserProfile (id: string) {
     const axiosInstance = axios.create();
 
     const intercep_auth_id = this.authIntercepService.addAuthInterceptor(axiosInstance);
     const intercep_error_id = this.authIntercepService.addAuthErrorInterceptor(axiosInstance);
 
-    axiosInstance.get(
+    await axiosInstance.get(
       environment.MYRMEX_API + '/perfil/apodo/' + id,
     )
       .then(response => {
@@ -124,13 +135,13 @@ export class PageProfileComponent implements OnInit {
   /**
   * Do signout from API
   */
-  async doSignout ( ) {
+  public async doSignout ( ) {
     const axiosInstance = axios.create();
 
     const intercep_auth_id = this.authIntercepService.addAuthInterceptor(axiosInstance);
     const intercep_error_id = this.authIntercepService.addAuthErrorInterceptor(axiosInstance);
 
-    axiosInstance.get(
+    await axiosInstance.get(
       environment.MYRMEX_API + '/sigul_sale',
     )
       .then(response => {
@@ -138,6 +149,76 @@ export class PageProfileComponent implements OnInit {
 
         if (response.data) {
           this.authIntercepService.delAuth();
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      })
+      .finally(( ) => {
+        this.authIntercepService.removeAuthInterceptor(
+          axiosInstance,
+          intercep_auth_id
+        );
+
+        this.authIntercepService.removeAuthErrorInterceptor(
+          axiosInstance,
+          intercep_error_id
+        );
+      });
+  }
+
+  /**
+   * Load profile permissions from API
+   */
+  private async loadProfilePermissions ( ) {
+    const axiosInstance = axios.create();
+
+    const intercep_auth_id = this.authIntercepService.addAuthInterceptor(axiosInstance);
+    const intercep_error_id = this.authIntercepService.addAuthErrorInterceptor(axiosInstance);
+
+    await axiosInstance.get(
+      environment.MYRMEX_API + '/permisos/perfil',
+    )
+      .then(response => {
+        console.log(response.data);
+
+        if (response.data && response.data.roles) {
+          this.permisos = this.buildPermisos(response.data.roles);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      })
+      .finally(( ) => {
+        this.authIntercepService.removeAuthInterceptor(
+          axiosInstance,
+          intercep_auth_id
+        );
+
+        this.authIntercepService.removeAuthErrorInterceptor(
+          axiosInstance,
+          intercep_error_id
+        );
+      });
+  }
+
+  /**
+   * Load user profile permissions from API
+   */
+  private async loadUserProfilePermissions (id: string) {
+    const axiosInstance = axios.create();
+
+    const intercep_auth_id = this.authIntercepService.addAuthInterceptor(axiosInstance);
+    const intercep_error_id = this.authIntercepService.addAuthErrorInterceptor(axiosInstance);
+
+    await axiosInstance.get(
+      environment.MYRMEX_API + '/permisos/perfil/apodo/' + id,
+    )
+      .then(response => {
+        console.log(response.data);
+
+        if (response.data && response.data.roles) {
+          this.permisos = this.buildPermisos(response.data.roles);
         }
       })
       .catch(error => {
@@ -178,6 +259,17 @@ export class PageProfileComponent implements OnInit {
 
   public esCerrarSesionVisible ( ): boolean {
     return this.tipo_vista === UsuarioPerfilVistaHabilitada.profile;
+  }
+
+  private buildPermisos (permisos_data: any): PermisoTipado[] {
+    const permisos: PermisoTipado[] = [];
+    permisos_data.forEach((permiso_data: any) => {
+      const permiso = FactoriaPermisosTipados.crearPermiso(permiso_data);
+      if (permiso) {
+        permisos.push(permiso);
+      }
+    });
+    return permisos;
   }
 
 }
