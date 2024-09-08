8952e796f752f48343b7612ea055d968baf4dbca
PedroBarr <pebarreraa@udistrital.edu.co>
2024-09-08 01:36:27 -0500

    Agregar funcionalidad obtener acciones de problema

--- src/app/features/feature_optimizadores/pages/page-explorer-problem/page-explorer-problem.component.ts
+++ src/app/features/feature_optimizadores/pages/page-explorer-problem/page-explorer-problem.component.ts
@@ -8,8 +8,16 @@ import { Router, ActivatedRoute } from '@angular/router';
 import axios from 'axios';
 
 import {
+  AutentificacionInterceptorService,
+} from 'src/app/shared/guards/auth.guard';
+
+import {
   Problema,
 } from '../../models/optimizador.model';
+
+import {
+  Acciones,
+} from '../../models/acciones.model';
 
 import { environment } from 'src/environments/environment';
 
@@ -22,14 +30,17 @@ import { environment } from 'src/environments/environment';
 export class PageExplorerProblemComponent implements OnInit {
 
   problema: Problema = new Problema();
+  acciones: Acciones = new Acciones();
 
   constructor (
     private router: Router,
     private route: ActivatedRoute,
+    private authIntercepService: AutentificacionInterceptorService,
   ) { }
 
-  ngOnInit ( ) {
-    this.loadProblem();
+  async ngOnInit ( ) {
+    await this.loadProblem();
+    await this.loadActions();
   }
 
   /**
@@ -75,6 +86,45 @@ export class PageExplorerProblemComponent implements OnInit {
         console.error(error);
       })
       .finally(( ) => { });
+  }
+
+  /**
+   * Load actions from API
+   */
+  async loadActions ( ) {
+    const axiosInstance = axios.create();
+
+    const intercep_auth_id = this.authIntercepService.addAuthInterceptor(axiosInstance);
+    const intercep_error_id = this.authIntercepService.addAuthErrorInterceptor(axiosInstance);
+
+    await axiosInstance.get(
+      environment.MYRMEX_API +
+        '/problema/identificador/' +
+        this.route.snapshot.paramMap.get('identificador') +
+        '/acciones',
+    )
+      .then(response => {
+        console.log(response.data);
+
+        if (response.data) {
+          this.acciones.fill_obj(response.data);
+        }
+
+      })
+      .catch(error => {
+        console.error(error);
+      })
+      .finally(( ) => {
+        this.authIntercepService.removeAuthInterceptor(
+          axiosInstance,
+          intercep_auth_id
+        );
+
+        this.authIntercepService.removeAuthErrorInterceptor(
+          axiosInstance,
+          intercep_error_id
+        );
+      });
   }
 
 }
