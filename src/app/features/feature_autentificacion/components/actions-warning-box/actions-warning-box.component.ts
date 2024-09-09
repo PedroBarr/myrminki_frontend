c7a5e143c06e7d3a4086b5af3550a901477afcee
PedroBarr <pebarreraa@udistrital.edu.co>
2024-09-09 10:02:17 -0500

    Corregir diseño caja de advertencia

--- src/app/features/feature_autentificacion/components/actions-warning-box/actions-warning-box.component.ts
+++ src/app/features/feature_autentificacion/components/actions-warning-box/actions-warning-box.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import {
+  ruta_inicio_sesion,
+  AutentificacionInterceptorService,
+} from 'src/app/shared/guards/auth.guard';
+
 @Component({
   selector: 'myrmex-actions-warning-box',
   templateUrl: './actions-warning-box.component.html',
@@ -7,9 +14,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionsWarningBoxComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private autentificacion: AutentificacionInterceptorService,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  private esAutenticado(): boolean {
+    return this.autentificacion.getAuth() !== null;
+  }
+
+  /**
+   * Funcion que permite definir el modo de renderizado
+   * de la caja de advertencia.
+   * 
+   * 0: Modo de advertencia inicie sesion para ver contenido.
+   * 1: Modo de advertencia solicite permisos para ver contenido.
+   * 
+   * @returns {Number} Modo de renderizado.
+   */
+  public modoRenderizado(): Number {
+    if (this.esAutenticado()) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  public irInicioSesion(): void {
+    this.router.navigate([ruta_inicio_sesion]);
   }
 
 }
