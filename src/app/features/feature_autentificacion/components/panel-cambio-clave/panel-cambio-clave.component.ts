78699a58ae995911b0b9cf4ad1d6ec0040eec18c
PedroBarr <pebarreraa@udistrital.edu.co>
2024-09-10 23:12:27 -0500

    Agregar funcionalidad y diseño de cambio clave

--- src/app/features/feature_autentificacion/components/panel-cambio-clave/panel-cambio-clave.component.ts
+++ src/app/features/feature_autentificacion/components/panel-cambio-clave/panel-cambio-clave.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+
+import axios from 'axios';
+
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'myrmex-panel-cambio-clave',
@@ -7,9 +18,124 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelCambioClaveComponent implements OnInit {
 
+  clave: string = '';
+  confirmacion: string = '';
+
+  clave_visible: boolean = false;
+  confirmacion_visible: boolean = false;
+
+  mensaje: string = '';
+  es_error: boolean = false;
+
+  @Input() simbolismo: string | null = null;
+  @Input() es_emitible: boolean = false;
+  @Output() emitir_datos_validados = new EventEmitter<any>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  verificar ( ) {
+    this.ocultarMensaje();
+
+    if (this.clave && this.confirmacion) {
+      let dict = {};
+
+      if (this.esSimbolico())
+        dict = {'simbolismo': this.simbolismo};
+
+      if (this.esConfirmacionValida())
+        dict = {'clave': this.clave, 'confirmacion': this.confirmacion};
+      else {
+        this.asignarError('Las claves no coinciden');
+        return;
+      }
+
+      if (this.es_emitible) this.emitir_datos_validados.emit(dict);
+      else this.doSwapup(dict);
+
+    } else {
+      this.asignarError('Complete los datos');
+    }
+  }
+
+  esConfirmacionValida ( ): boolean {
+    return this.clave === this.confirmacion;
+  }
+
+  esMensajeVisible ( ): boolean {
+    return !(this.mensaje == '');
+  }
+
+  ocultarMensaje ( ) {
+    this.mensaje = '';
+    this.es_error = false;
+  }
+
+  asignarError (error: string) {
+    this.mensaje = error;
+    this.es_error = true;
+  }
+
+  asignarExito (exito: string) {
+    this.mensaje = exito;
+    this.es_error = false;
+  }
+
+  obtener_clase_mensaje ( ): string {
+    return this.es_error ? 'alert-danger' : 'alert-success';
+  }
+
+  esSimbolico ( ) {
+    return this.simbolismo !== '' && this.simbolismo !== null;
+  }
+
+  tituloComponente ( ) {
+    return this.esSimbolico() ? 'Recuperación de clave' : 'Cambio de clave';
+  }
+
+  subtituloComponente ( ) {
+    return this.esSimbolico() ? 'Ingrese una nueva clave' : 'Ingrese su nueva clave y valide la anterior';
+  }
+
+  botonComponente ( ) {
+    return this.esSimbolico() ? 'Recuperar' : 'Cambiar';
+  }
+
+  voltearClaveVisible ( ) {
+    this.clave_visible = !this.clave_visible;
+  }
+
+  voltearConfirmacionVisible ( ) {
+    this.confirmacion_visible = !this.confirmacion_visible;
+  }
+
+  /**
+  * Do swapup from API
+  */
+  async doSwapup (obj: any) {
+    await axios.post(
+      environment.MYRMEX_API + '/sigul_reconfigurar',
+      obj,
+    )
+      .then(response => {
+        console.log(response.data);
+
+        if (response.data['Respuesta'])
+          this.asignarExito(response.data['Respuesta']);
+        else
+          this.asignarError('No se pudo cambiar la clave');
+      })
+      .catch(error => {
+        console.error(error);
+
+        if (error.response && error.response.data)
+          this.asignarError(Object.keys(error.response.data).join(', '));
+        else
+          this.asignarError('No se pudo cambiar la clave');
+      })
+      .finally(( ) => { });
   }
 
 }
