b610e8c356970c7a114f0f7d5917f3a21a8c27d1
PedroBarr <pebarreraa@udistrital.edu.co>
2024-09-12 22:42:50 -0500

    Agregar diseño funcionalidad de acciones de referentes

--- src/app/features/feature_comunidad/components/academic-referecnce-box/academic-referecnce-box.component.ts
+++ src/app/features/feature_comunidad/components/academic-referecnce-box/academic-referecnce-box.component.ts
@@ -15,5 +15,15 @@ import { AcademicReference } from '../../models/academic-reference.model';
 export class AcademicReferecnceBoxComponent {
 
   @Input() academicReference: AcademicReference = <AcademicReference>{};
+  @Input() es_reportable: boolean = false;
+  @Input() es_eliminable: boolean = false;
+
+  public esMenuVisible ( ) {
+    return (
+      this.es_reportable ||
+      this.es_eliminable ||
+      false
+    );
+  }
 
 }
--- src/app/features/feature_comunidad/components/components.module.ts
+++ src/app/features/feature_comunidad/components/components.module.ts
@@ -9,6 +9,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 
 import {
   AcademicReferecnceBoxComponent
@@ -35,6 +36,7 @@ import {
     MatChipsModule,
     MatIconModule,
     MatListModule,
+    MatMenuModule,
 
     RouterModule,
   ],
