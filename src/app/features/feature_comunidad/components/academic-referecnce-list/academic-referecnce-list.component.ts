fbb11f65cc620eeb2d558b5be9f90fd11d39f6e7
PedroBarr <pebarreraa@udistrital.edu.co>
2024-09-17 19:00:53 -0500

    Refactorizar lista de referentes

--- src/app/features/feature_comunidad/components/academic-referecnce-list/academic-referecnce-list.component.ts
+++ src/app/features/feature_comunidad/components/academic-referecnce-list/academic-referecnce-list.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+
+import {
+  AcademicReference,
+} from '../../models/academic-reference.model';
+
+import {
+  Acciones,
+} from '../../../feature_optimizadores/models/acciones.model';
 
 @Component({
   selector: 'myrmex-academic-referecnce-list',
@@ -7,9 +21,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcademicReferecnceListComponent implements OnInit {
 
+  private refertente_seleccionado: AcademicReference | null = null;
+  
+  @Input() academicReferences: AcademicReference[] = [];
+
+  @Input() es_reportable: boolean = false;
+  @Input() es_editable: boolean = false;
+  @Input() es_eliminable: boolean = false;
+
+  @Output() emitirSeleccion: EventEmitter<AcademicReference> = 
+    new EventEmitter<AcademicReference>();
+
+  @Output() emitirEditarReferente: EventEmitter<AcademicReference> =
+    new EventEmitter<AcademicReference>();
+
+  @Output() emitirEliminarReferente: EventEmitter<AcademicReference> =
+    new EventEmitter<AcademicReference>();
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  editarReferente (refrt: AcademicReference | null) {
+    let refrt_selecto = refrt ? refrt : new AcademicReference();
+    this.emitirEditarReferente.emit(refrt_selecto);
+  }
+
+  eliminarReferente (refrt: AcademicReference) {
+    this.emitirEliminarReferente.emit(refrt);
   }
 
 }
